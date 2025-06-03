1b62d0e082731398d92bb918eea1533c43c2123d
caleb <calebs.phone2017@gmail.com>
2025-06-03 14:35:52 -0400

    Created form and applied it to the app, now it allows users to create messages to the app

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
 from .models import *
+from .forms import ChatmessageCreateFrom
 
 # Create your views here.
 
@@ -13,6 +14,31 @@ def chat_view(request):
     # get the last 30 messages within the group
     chat_messages = chat_group.chat_messages.all()[:30]
 
+    # this gets the form to create new messages into out group chat app
+    form = ChatmessageCreateFrom()
+
+    # if the user it making a post
+    if request.method == 'POST':
+
+        # select the form
+        form = ChatmessageCreateFrom(request.POST)
+
+        # check if the form is valid
+        if form.is_valid:
+
+            # define the message
+            message = form.save(commit=False)
+
+            # assign the messages author and group
+            message.author = request.user
+            message.group = chat_group
+
+            # save the message
+            message.save()
+
+            return redirect("home")
+
     # uses chat.html in templates/a_rtchat as the template
-    return render(request, 'a_rtchat/chat.html',{'chat_messages' : chat_messages})
+    return render(request, 'a_rtchat/chat.html',
+                  {'chat_messages' : chat_messages, 'form' : form})
 
